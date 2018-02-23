8956cea7065bdb6abb86eea762a832d028126817
sainag2473 <immidisetty.2@wright.edu>
2018-02-22 19:30:13 -0500

    Tested data Analysis
    still pending

---
+++ OpenModal Export CSV -- 2017 17-11 12-15-17/NewModel_0/measurements/wing/vtb10.m
@@ -0,0 +1,10 @@
+% Directory of missing chapter 10 toolbox codes.
+%
+% VTB10_1 plots the phase plan of a second order differential
+%         equation using finite differences.  See VTB10ex.
+% VTB10ex example function file for VTB10_1.  Type help 
+%         VTB10_1 for instructions on running this example.
+
+
+%Automatically check for updates
+vtbchk
