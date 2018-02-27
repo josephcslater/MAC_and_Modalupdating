7903c7c336d6118d95384dbc7010cc82a0611055
sainag2473 <immidisetty.2@wright.edu>
2018-02-26 21:53:31 -0500

    corrections

--- Wing_FE_Model/WFEM-master/WFEM/modalanalysis.m
+++ Wing_FE_Model/WFEM-master/WFEM/modalanalysis.m
@@ -45,7 +45,7 @@ if isempty(fs)|restart==0,% This is old stuff from previous restart
 %master1=[3;9;15;21;27;33;39;45;51;57;63;69;75;81;87;93;99;105;111;117;123;129;135;141;147;153;159;165;171;177;183;189;195;201;207;213;219;225;231;237;243;249]';
 %length(master1)
 %[1;2;3;7;8;9;13;14;15;19;20;21;25;26;27;31;32;33;37;38;39;43;44;45;49;50;51;55;56;57;61;62;63;67;68;69;73;74;75;79;80;81;85;86;87;91;92;93;97;98;99;103;104;105;109;110;111;115;116;117;121;122;123;127;128;129;133;134;135;139;140;141;145;146;147;151;152;153;157;158;159;163;164;165;169;171;171;175;176;177;181;182;183;187;188;189;193;194;195;199;200;201;205;206;207;211;212;213;217;218;219;223;224;225;229;230;231;235;236;237;241;242;243;247;248;249]'
-master2=[1:8];
+%master2=[1:8];
 master1=i(lslave+1:size(nodes,1)*6)%The master coordinates are
 %master1=[1:108];                                       %the ones that we didn't
                                        %reduce out via some
@@ -58,8 +58,8 @@ master1=i(lslave+1:size(nodes,1)*6)%The master coordinates are
                                        %DOFS. The upper limit of
                                        %size(nodes,1)*6 fixes this
                                        %at the last real DOF.
-   [Mr,Kr,T,master,slave]=guyan(M,K,master1)
-[Mred,Kred,T,master,slave]=serep(Mr,Kr,master2)
+   [Mred,Kred,T,master,slave]=guyan(M,K,master1)
+%[Mred,Kred,T,master,slave]=serep(Mr,Kr,master2)
   else
     [Mr,Kr,T,master,slave]=guyan(M,K)
     
