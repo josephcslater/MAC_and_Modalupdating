3b0997fb9b4511306b5e6c315dec6594dff68392
sainag2473 <immidisetty.2@wright.edu>
2018-06-24 12:00:13 -0400

    Tests for Validity of Data

---
+++ MAC_JupyterNotebook/reciprocity/hammerat6&accelat7/Untitled.m
@@ -0,0 +1,2 @@
+ load('C:\Users\ecslogon\Desktop\reciprocity\accelat6&hammerat7\1.mat');plot(Freq_domain,20*log10(Hf_chan_2),'r');hold on;
+ load('C:\Users\ecslogon\Desktop\reciprocity\hammerat6&accelat7\1-1.mat');plot(Freq_domain,20*log10(Hf_chan_2),'y');
